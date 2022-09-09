c0bba268f3ed363f108fcaffd6944e2fbba0dd1a
nhorta-g <101281946+nhorta-g@users.noreply.github.com>
2022-09-09 19:10:36 +0100

    Add files via upload

--- inc/so_long.h
+++ inc/so_long.h
@@ -6,14 +6,26 @@
 /*   By: nhorta-g <nhorta-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 12:02:21 by nhorta-g          #+#    #+#             */
-/*   Updated: 2022/08/23 12:04:50 by nhorta-g         ###   ########.fr       */
+/*   Updated: 2022/09/09 17:54:22 by nhorta-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+#include "../mlx/mlx.h"
+
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	end_and_error_msg(char *msg);
 
 #endif
