c7b2e0b883595ec7bee5680c4456df1b8adb04f0
Nuno Grazina <101281946+nhorta-g@users.noreply.github.com>
2022-09-18 12:21:39 +0100

    Add files via upload

---
+++ inc/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nuno <nuno@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/14 19:08:42 by nhorta-g          #+#    #+#             */
+/*   Updated: 2022/09/16 16:48:45 by nuno             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+
+#endif
--- inc/so_long.h
+++ inc/so_long.h
@@ -3,22 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nhorta-g <nhorta-g@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nuno <nuno@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 12:02:21 by nhorta-g          #+#    #+#             */
-/*   Updated: 2022/09/09 17:54:22 by nhorta-g         ###   ########.fr       */
+/*   Updated: 2022/09/16 17:43:33 by nuno             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-#include "../mlx/mlx.h"
+#include "../mlx_linux/mlx.h"
+#include "../libft/libft.h"
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
-typedef struct	s_data {
+typedef struct	s_data
+{
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
@@ -26,6 +29,16 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
-void	end_and_error_msg(char *msg);
+typedef struct	s_game
+{
+	int		fd;
+}				t_game;
+
+void	init_map(char *map_file_name, t_game *game);
+void	check_arg(int argc, char *argv);
+void	check_wall(char *line);
+void	error_msg_exit(char *message);
+void	error_msg_free_exit(char *message, char *to_free);
+char	*get_next_line(int fd);
 
 #endif
